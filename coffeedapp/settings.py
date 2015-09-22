a59d58e003b5f68b5f149bc48600c5eda2fb332a
Christophe Garconnet <iMac@iMac-de-Christophe.local>
2015-09-23 00:25:45 +0200

    solving again

--- coffeedapp/settings.py
+++ coffeedapp/settings.py
@@ -86,8 +86,8 @@ if ON_HEROKU == '1':
 
     # Parse database configuration from $DATABASE_URL
     import dj_database_url
-    DATABASES['default'] = dj_database_url.config()
-#    DATABASES = {'default' : dj_database_url.config()}
+#    DATABASES['default'] = dj_database_url.config()
+    DATABASES = {'default' : dj_database_url.config()}
 
 
 else: 
